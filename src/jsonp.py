6ad7249cc655fb4fddf83ab7a5e02f7c3a01bc48
Tomas Vondra <tomas@pgaddict.com>
2015-01-01 00:18:36 +0100

    improved sanitization of the JSONP callback (don't sanitize empty values, fix pattern)

--- src/jsonp.py
+++ src/jsonp.py
@@ -13,11 +13,11 @@ def support_jsonp(f):
 		callback = request.args.get('callback', False)
 
 		# sanitize the callback names
-		if not re.match('^pgxn_[a-zA-Z0-9-]+$', callback):
-			callback = None
+		if callback and not re.match('^pgxn_[a-zA-Z0-9_]+$', str(callback)):
+			callback = False
 
 		if callback:
-			content = str(callback) + '(' + str(f(*args,**kwargs).data) + ')'
+			content = str(callback) + '(' + str(f(*args,**kwargs).data).strip() + ')'
 			return current_app.response_class(content, mimetype='application/javascript')
 		else:
 			return f(*args, **kwargs)
